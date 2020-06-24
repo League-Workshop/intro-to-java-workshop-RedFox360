ccc92a8aab03ad460dc479ac0b7461fe4ff954e0
root <root@cf94a469289d>
2020-06-24 18:58:52 +0000

    Day 3 6/24/20

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,15 +1,22 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+int score = 0;
 		// 2.  Ask the user a question 
-		
+		String answer1 = JOptionPane.showInputDialog("When was Java created?");
+
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(answer1.equals("yes"))	{
+			score+=1;
+		}
+		else	{
+		}
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
